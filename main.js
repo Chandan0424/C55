326527ef37ebbd1e3b26bb800511ba0266c4d485
Chandan0424 <76838260+Chandan0424@users.noreply.github.com>
2021-01-11 17:39:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function Spanelement(){
+    window.alert("This is a span element");
+    document.getElementById("audio").play();
+}
+function Divelement(){
+    window.alert("This is a div element");
+}
